7fcb89cc96e39a444b246dda41627a56a747a238
Coronus117 <samkushner919@gmail.com>
2023-02-22 16:22:11 -0500

    Revert "Hide mobile button sticky hover"
    This reverts commit b8727bc4b52972e825638aaac8467cc92a09e6bc.

--- components/pump-vote-bar/pump-vote-bar.js
+++ components/pump-vote-bar/pump-vote-bar.js
@@ -4,6 +4,7 @@ import { getCurrentExerciseRoutine, getFilteredExercise } from "@/exercises";
 
 const PumpVoteBar = () => {
   const currentExRoutine = getCurrentExerciseRoutine();
+  console.log("currentExRoutine ", currentExRoutine);
   return (
     <div className="bg-white rounded-lg p-4 border-2 border-gray-200 space-y-3">
       <div>Vote on exercises for the next show</div>
--- components/ui/button.js
+++ components/ui/button.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Button = ({ children }) => {
   return (
-    <button className="bg-gray-300 p-4 rounded border-2 select-none border-gray-300 hover:bg-gray-400 active:bg-white rounded-md focus:bg-gray-300">
+    <div className="bg-gray-300 p-4 rounded border-2 select-none border-gray-300 hover:bg-gray-400 active:bg-white rounded-md">
       {children}
-    </button>
+    </div>
   );
 };
 
